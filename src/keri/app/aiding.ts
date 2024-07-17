a4dc8524b8f096dcbe6ebae538df8afa95cd899f
leo.do <leo.do@sotatek.com>
2024-07-17 10:36:15 +0700

    DTIS-982: update the cnfg and data for incept event message

--- src/keri/app/aiding.ts
+++ src/keri/app/aiding.ts
@@ -20,6 +20,7 @@ export interface CreateIdentiferArgs {
     delpre?: string;
     dcode?: string;
     data?: any;
+    cnfg?: any[];
     algo?: Algos;
     pre?: string;
     states?: any[];
@@ -163,6 +164,7 @@ export class Identifier {
         const proxy = kargs.proxy;
         const delpre = kargs.delpre;
         const data = kargs.data != undefined ? [kargs.data] : [];
+        const cnfg = kargs.cnfg != undefined ? kargs.cnfg : [];
         const pre = kargs.pre;
         const states = kargs.states;
         const rstates = kargs.rstates;
@@ -223,7 +225,7 @@ export class Identifier {
                 nsith: nsith,
                 toad: toad,
                 wits: wits,
-                cnfg: [],
+                cnfg: cnfg,
                 data: data,
                 version: Versionage,
                 kind: Serials.JSON,
@@ -238,7 +240,7 @@ export class Identifier {
                 nsith: nsith,
                 toad: toad,
                 wits: wits,
-                cnfg: [],
+                cnfg: cnfg,
                 data: data,
                 version: Versionage,
                 kind: Serials.JSON,
